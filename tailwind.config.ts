a227d561f643a736ea16fe1289dfbe532794e800
nizar <nizarrr.id@gmail.com>
2025-01-11 12:05:04 +0700

    Add utility functions and update homepage structure with Tailwind CSS integration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,13 @@ const config: Config = {
 			colors: {
 				primary: {
 					"100": "#FFE8F0",
-					DEFAULT: "#EE2B69",
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
 				},
-				secondary: "#FBE843",
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
 				black: {
 					"100": "#333333",
 					"200": "#141413",
@@ -24,6 +28,38 @@ const config: Config = {
 				white: {
 					"100": "#F7F7F7",
 					DEFAULT: "#FFFFFF",
+				},
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				chart: {
+					"1": "hsl(var(--chart-1))",
+					"2": "hsl(var(--chart-2))",
+					"3": "hsl(var(--chart-3))",
+					"4": "hsl(var(--chart-4))",
+					"5": "hsl(var(--chart-5))",
 				},
 			},
 			fontFamily: {
@@ -35,9 +71,9 @@ const config: Config = {
 				sm: "calc(var(--radius) - 4px)",
 			},
 			boxShadow: {
-				100: "2px 2px 0px 0px rgb(0, 0, 0)",
-				200: "2px 2px 0px 2px rgb(0, 0, 0)",
-				300: "2px 2px 0px 2px rgb(238, 43, 105)",
+				"100": "2px 2px 0px 0px rgb(0, 0, 0)",
+				"200": "2px 2px 0px 2px rgb(0, 0, 0)",
+				"300": "2px 2px 0px 2px rgb(238, 43, 105)",
 			},
 		},
 	},
